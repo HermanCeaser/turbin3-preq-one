3befbe275e699be051bbbc9a6601f7febc70690c
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-08 12:38:34 +0300

    added program IDL

--- keygen.ts
+++ keygen.ts
@@ -5,3 +5,5 @@ let kp = Keypair.generate();
 console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`)
 
 console.log(`[${kp.secretKey}]`);
+
+// HspiNMpPnK7rkxYnECbj3GsFLwxhQw8vs29rPniv4ja1
